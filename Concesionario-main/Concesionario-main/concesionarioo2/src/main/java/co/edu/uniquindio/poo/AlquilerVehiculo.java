db416aa141bd575e2313c0260e80a7cdd33c5296
Sofiasnta <sofia.santamariar@uqvirtual.edu.co>
2024-11-16 17:32:06 -0500

    Update AlquilerVehiculo.java

--- Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/AlquilerVehiculo.java
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/AlquilerVehiculo.java
@@ -8,15 +8,19 @@ public class AlquilerVehiculo {
     private String codigo;
     private int diasAlquiler;
     private int precioAlquilerPorDia;
-    private Collection <Vehiculo> vehiculos;
+    private VehiculoGasolinaDiesel VehiculoGasolinaDiesel;
+    private VehiculoHibrido vehiculoHibrido;
+    private VehiculoElectrico vehiculoElectrico;
     private Empleado empleado; 
     private Cliente cliente;
 
-    public AlquilerVehiculo (String codigo, int diasAlquiler, int precioAlquilerPorDia, Empleado emplead, Cliente cliente){
+    public AlquilerVehiculo (String codigo, int diasAlquiler, int precioAlquilerPorDia,  VehiculoGasolinaDiesel VehiculoGasolinaDiesel, VehiculoHibrido vehiculoHibrido, VehiculoElectrico vehiculoElectrico, Empleado empleado, Cliente cliente){
         this.codigo = codigo;
         this.diasAlquiler = diasAlquiler;
         this.precioAlquilerPorDia = precioAlquilerPorDia;
-        this.vehiculos = new LinkedList<>();
+        this.vehiculoGasolinaDiesel = vehiculoGasolinaDiesel;
+        this.vehiculoHibrido = vehiculoHibrido;
+        this.vehiculoElectrico = vehiculoElectrico;
         this.empleado = empleado;
         this.cliente = cliente;
     }
@@ -45,12 +49,29 @@ public class AlquilerVehiculo {
         this.precioAlquilerPorDia = precioAlquilerPorDia;
     }
 
-    public Collection<Vehiculo> getVehiculos() {
-        return vehiculos;
+
+    public VehiculoGasolinaDiesel getVehiculoGasolinaDiesel() {
+        return VehiculoGasolinaDiesel;
     }
 
-    public void setVehiculos(Collection<Vehiculo> vehiculos) {
-        this.vehiculos = vehiculos;
+    public void setVehiculoGasolinaDiesel(VehiculoGasolinaDiesel vehiculoGasolinaDiesel) {
+        VehiculoGasolinaDiesel = vehiculoGasolinaDiesel;
+    }
+
+    public VehiculoHibrido getVehiculoHibrido() {
+        return vehiculoHibrido;
+    }
+
+    public void setVehiculoHibrido(VehiculoHibrido vehiculoHibrido) {
+        this.vehiculoHibrido = vehiculoHibrido;
+    }
+
+    public VehiculoElectrico getVehiculoElectrico() {
+        return vehiculoElectrico;
+    }
+
+    public void setVehiculoElectrico(VehiculoElectrico vehiculoElectrico) {
+        this.vehiculoElectrico = vehiculoElectrico;
     }
 
     public Empleado getEmpleado() {
