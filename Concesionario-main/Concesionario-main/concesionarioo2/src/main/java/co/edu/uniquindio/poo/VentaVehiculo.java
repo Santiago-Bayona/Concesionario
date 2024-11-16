3ad792349e050a05c64e7528b14ce47681b92230
Sofiasnta <sofia.santamariar@uqvirtual.edu.co>
2024-11-16 17:00:53 -0500

    Update VentaVehiculo.java

--- Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/VentaVehiculo.java
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/VentaVehiculo.java
@@ -1,14 +1,22 @@
 package co.edu.uniquindio.poo;
+import java.util.Collection;
+import java.util.LinkedList;
 
 public class VentaVehiculo {
 
     //collection VEHICULOS
     private String codigo;
     private int precioVenta;
+    private Collection <Vehiculo> vehiculos;
+    private Empleado empleado; 
+    private Cliente cliente;
 
-    public  VentaVehiculo (String codigo, int precioVenta){
+    public  VentaVehiculo (String codigo, int precioVenta, Empleado empleado, Cliente cliente){
         this.codigo = codigo;
         this.precioVenta = precioVenta;
+        this.vehiculos = new LinkedList<>();
+        this.empleado = empleado;
+        this.cliente = cliente;
     }
 
     public String getCodigo() {
@@ -27,8 +35,38 @@ public class VentaVehiculo {
         this.precioVenta = precioVenta;
     }
 
+
+    public Collection<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(Collection<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
     @Override
     public String toString() {
         return "VentaVehiculo [codigo=" + codigo + ", precioVenta=" + precioVenta + "]";
     }
+
+    
+
+
+
 }
