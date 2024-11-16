63aa62af3b2110055be505d0e680aa69669093be
Sofiasnta <sofia.santamariar@uqvirtual.edu.co>
2024-11-16 15:51:13 -0500

    Update AlquilerVehiculo.java

--- Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/AlquilerVehiculo.java
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/AlquilerVehiculo.java
@@ -1,15 +1,24 @@
 package co.edu.uniquindio.poo;
 
+import java.util.LinkedList;
+import java.util.Collection;
+
 public class AlquilerVehiculo {
     
     private String codigo;
     private int diasAlquiler;
     private int precioAlquilerPorDia;
+    private Collection <Vehiculo> vehiculos;
+    private Empleado empleado; 
+    private Cliente cliente;
 
-    public AlquilerVehiculo (String codigo, int diasAlquiler, int precioAlquilerPorDia){
+    public AlquilerVehiculo (String codigo, int diasAlquiler, int precioAlquilerPorDia, Empleado emplead, Cliente cliente){
         this.codigo = codigo;
         this.diasAlquiler = diasAlquiler;
         this.precioAlquilerPorDia = precioAlquilerPorDia;
+        this.vehiculos = new LinkedList<>();
+        this.empleado = empleado;
+        this.cliente = cliente;
     }
 
     public String getCodigo() {
@@ -34,6 +43,30 @@ public class AlquilerVehiculo {
 
     public void setPrecioAlquilerPorDia(int precioAlquilerPorDia) {
         this.precioAlquilerPorDia = precioAlquilerPorDia;
+    }
+
+    public Collection<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(Collection<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
     }
 
     @Override
