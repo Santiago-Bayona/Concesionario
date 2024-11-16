9d2dd9f47a394016f63cf90d05e7209b748f339a
Sofiasnta <sofia.santamariar@uqvirtual.edu.co>
2024-11-16 15:51:41 -0500

    Update CompraVehiculo.java

--- Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/CompraVehiculo.java
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/CompraVehiculo.java
@@ -1,15 +1,23 @@
 package co.edu.uniquindio.poo;
+import java.util.Collection;
+import java.util.LinkedList;
 
 public class CompraVehiculo {
 
     private String codigo;
     private int precioVenta;
     private boolean revisionTecnicaAprobado;
+    private Collection <Vehiculo> vehiculos;
+    private Empleado empleado; 
+    private Cliente cliente;
 
-    public CompraVehiculo (String codigo, int precioVenta, boolean revisionTecnicaAprobado){
+    public CompraVehiculo (String codigo, int precioVenta, boolean revisionTecnicaAprobado, Empleado empleado, Cliente cliente){
         this.codigo = codigo;
         this.precioVenta = precioVenta;
         this.revisionTecnicaAprobado = revisionTecnicaAprobado;
+        this.vehiculos = new LinkedList<>();
+        this.empleado = empleado;
+        this.cliente = cliente;
     }
 
     public String getCodigo() {
@@ -34,6 +42,31 @@ public class CompraVehiculo {
 
     public void setRevisionTecnicaAprobado(boolean revisionTecnicaAprobado) {
         this.revisionTecnicaAprobado = revisionTecnicaAprobado;
+    }
+
+
+    public Collection<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(Collection<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
     }
 
     @Override
