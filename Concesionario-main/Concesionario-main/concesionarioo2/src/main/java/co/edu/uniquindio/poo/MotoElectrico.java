6d56abaefd178ab2ebd8932063a3c12a10262e3a
Andres-eng728 <andresm.zapatal@uqvirtual.edu.co>
2024-11-16 16:54:03 -0500

    Add files via upload

---
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/MotoElectrico.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.poo;
+
+public class MotoElectrico extends VehiculoElectrico {
+
+
+    public MotoElectrico(String codigo,String marca,boolean estado,String modelo,int cambios,int velocidadmaxima,int cilindraje,String autonomiacargacompleta,int tiempopromediocarga,co.edu.uniquindio.poo.Vehiculo.TipoTransmision tipoTransmision) {
+        super(codigo,marca,estado,modelo,cambios,velocidadmaxima, cilindraje,autonomiacargacompleta,tiempopromediocarga,tipoTransmision);
+    }
+
+
+
+}
