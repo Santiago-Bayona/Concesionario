aba56a34e07727cc5af304830f6d3e8c8428e456
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2024-11-16 17:21:59 -0500

    Add files via upload

--- Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/Empresa.java
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/Empresa.java
@@ -7,30 +7,106 @@ public class Empresa {
     public String nombre;
     public Collection<Administrador>administradores;
     public Collection<Transaccion>transacciones;
+    public Collection<VentaVehiculo>ventas;
+    public Collection<CompraVehiculo>compras;
+    public Collection<AlquilerVehiculo>alquilerVehiculos;
+
     public Empresa(String nombre) {
         this.nombre = nombre;
         this.administradores = new LinkedList<>();
         this.transacciones = new LinkedList<>();
+        this.ventas = new LinkedList<>();
+        this.alquilerVehiculos = new LinkedList<>();
+        this.compras = new LinkedList<>();
     }
+    
+    
+    
+    
     public String getNombre() {
         return nombre;
     }
+
+
+
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+
+
+
     public Collection<Administrador> getAdministradores() {
         return administradores;
     }
+
+
+
+
     public void setAdministradores(Collection<Administrador> administradores) {
         this.administradores = administradores;
     }
+
+
+
+
     public Collection<Transaccion> getTransacciones() {
         return transacciones;
     }
+
+
+
+
     public void setTransacciones(Collection<Transaccion> transacciones) {
         this.transacciones = transacciones;
     }
-    
+
+
+
+
+    public Collection<VentaVehiculo> getVentas() {
+        return ventas;
+    }
+
+
+
+
+    public void setVentas(Collection<VentaVehiculo> ventas) {
+        this.ventas = ventas;
+    }
+
+
+
+
+    public Collection<CompraVehiculo> getCompras() {
+        return compras;
+    }
+
+
+
+
+    public void setCompras(Collection<CompraVehiculo> compras) {
+        this.compras = compras;
+    }
+
+
+
+
+    public Collection<AlquilerVehiculo> getAlquilerVehiculos() {
+        return alquilerVehiculos;
+    }
+
+
+
+
+    public void setAlquilerVehiculos(Collection<AlquilerVehiculo> alquilerVehiculos) {
+        this.alquilerVehiculos = alquilerVehiculos;
+    }
+
+
+
+
     public boolean verificarTransaccion(String codigo) {
         for (Transaccion transaccion : transacciones) {
             if (codigo.equals(transaccion.getCodigo())) {
@@ -78,9 +154,82 @@ public class Empresa {
             }
         }
     }
+
+    public boolean verificarVenta(String codigo) {
+        for (VentaVehiculo venta : ventas) {
+            if (codigo.equals(venta.getCodigo())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void agregarVenta(VentaVehiculo venta){
+        if(verificarVenta(venta.getCodigo())){
+            ventas.add(venta);
+        }
+    }
+
+    public void eliminarVenta(String codigo) {
+        for (VentaVehiculo venta : ventas) {
+            if (venta.getCodigo().equals(codigo)) {
+                ventas.remove(venta);
+                break;
+            }
+        }
+    }
+
+    public boolean verificarCompra(String codigo) {
+        for (CompraVehiculo compra : compras) {
+            if (codigo.equals(compra.getCodigo())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void agregarCompra(CompraVehiculo compraVehiculo){
+        if(verificarCompra(compraVehiculo.getCodigo())){
+            compras.add(compraVehiculo);
+        }
+    }
+
+    public void eliminarCompra(String codigo) {
+        for (CompraVehiculo compra : compras) {
+            if (compra.getCodigo().equals(codigo)) {
+                compras.remove(compra);
+                break;
+            }
+        }
+    }
+
+    public boolean verificarAlquiler(String codigo) {
+        for (AlquilerVehiculo Alquiler : alquilerVehiculos) {
+            if (codigo.equals(Alquiler.getCodigo())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void agregarAlquiler(AlquilerVehiculo Alquiler){
+        if(verificarAlquiler(Alquiler.getCodigo())){
+            alquilerVehiculos.add(Alquiler);
+        }
+    }
+
+    public void eliminarAlquiler(String codigo) {
+        for (AlquilerVehiculo Alquiler : alquilerVehiculos) {
+            if (Alquiler.getCodigo().equals(codigo)) {
+                alquilerVehiculos.remove(Alquiler);
+                break;
+            }
+        }
+    }
+
     @Override
     public String toString() {
         return "Empresa [nombre=" + nombre + ", administradores=" + administradores + ", transacciones=" + transacciones
-                + "]";
+                + ", ventas=" + ventas + ", compras=" + compras + ", alquilerVehiculos=" + alquilerVehiculos + "]";
     }
 }
