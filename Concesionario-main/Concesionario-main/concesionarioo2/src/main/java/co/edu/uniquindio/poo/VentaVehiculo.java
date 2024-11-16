7934a44550a79051fb60d9f99a10aeb3fa75f569
Sofiasnta <sofia.santamariar@uqvirtual.edu.co>
2024-11-16 17:01:23 -0500

    Update VentaVehiculo.java

--- Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/VentaVehiculo.java
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/VentaVehiculo.java
@@ -4,7 +4,6 @@ import java.util.LinkedList;
 
 public class VentaVehiculo {
 
-    //collection VEHICULOS
     private String codigo;
     private int precioVenta;
     private Collection <Vehiculo> vehiculos;
