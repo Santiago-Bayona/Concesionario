3b2dc5cc37d6dcbb6c77e95951bd7cbfef35b902
Andres-eng728 <andresm.zapatal@uqvirtual.edu.co>
2024-11-16 19:01:34 -0500

    Add files via upload

--- Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/BusElectrico.java
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/BusElectrico.java
@@ -1,12 +1,14 @@
 package co.edu.uniquindio.poo;
 
-public class BusElectrico extends CamionElectrico {
+public class BusElectrico extends VanElectrico {
     private int numeroSalidasEmergencia;
 
-    public BusElectrico(String codigo, String marca, boolean estado, String modelo, int cambios, int velocidadmaxima, int cilindraje,
-            String autonomiacargacompleta, int tiempopromediocarga, double capacidadCarga, boolean frenosAire, boolean ABS,
-            int numeroEjes, int numeroejes, int numeroSalidasEmergencia,co.edu.uniquindio.poo.Vehiculo.TipoTransmision tipoTransmision) {
-        super(codigo, marca, estado, modelo, cambios, velocidadmaxima, cilindraje, autonomiacargacompleta, tiempopromediocarga,capacidadCarga,ABS,frenosAire,numeroEjes,tipoTransmision);
+    public BusElectrico(String Placa, String marca, boolean nuevo, String modelo, int cambios, int velocidadMaxima,
+            int cilindraje, co.edu.uniquindio.poo.Vehiculo.TipoTransmision tipoTransmision, 
+             int numeroPasajeros, int numeropuertas, double capacidadMaletero,
+            boolean aireAcondicionado, boolean camaraReversa, int numeroBolsasAire, boolean aBS,
+            int numeroSalidasEmergencia, String autonomiacargacompleta, int tiempopromediocarga) {
+        super(Placa, marca, nuevo, modelo, cambios, velocidadMaxima, cilindraje, autonomiacargacompleta, tiempopromediocarga,  numeropuertas, aireAcondicionado,numeroPasajeros, camaraReversa, numeroBolsasAire, aBS, capacidadMaletero, tipoTransmision);
       
         this.numeroSalidasEmergencia = numeroSalidasEmergencia;
     }
