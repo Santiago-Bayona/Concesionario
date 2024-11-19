c9e7d6dc5032a3e8dfe13b8d53b62489517b3fe0
Andres-eng728 <andresm.zapatal@uqvirtual.edu.co>
2024-11-18 22:45:27 -0500

    Add files via upload

--- Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/App.java
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/App.java
@@ -1,6 +1,12 @@
 package co.edu.uniquindio.poo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import co.edu.uniquindio.poo.CamionGasolinaDiesel.TipoCamion;
 import co.edu.uniquindio.poo.Persona.PreguntaSeguridad;
+import co.edu.uniquindio.poo.Vehiculo.TipoTransmision;
+import co.edu.uniquindio.poo.VehiculoGasolinaDiesel.GasolinaDiesel;
 
 public class App {
     public static void main(String[] args) {
@@ -8,11 +14,27 @@ public class App {
         Empresa UQ = new Empresa("Concensionario UQ");
 
         Administrador administrador1 = new Administrador("jose", "gonzalez", "JoseG@gmail.com", "josesito123", "Chocolate", 23, "10248", PreguntaSeguridad.Cual_es_su_sabor_favoorito_de_heLado);
+        VentaVehiculo venta1 = new VentaVehiculo("1313",20000,null,null,null,null,null);
+        Transaccion transaccion1 = new Transaccion( "1231", LocalDate.of(2023,5,15),10002,true, null,null,null,null,null);
+        CompraVehiculo compra1 = new CompraVehiculo("2321",22121,true,null,null,null,null,null);
+        AlquilerVehiculo alquiler1 = new AlquilerVehiculo( "1231",32 ,10002,null,null,null,null,null);
+        Empleado empleado1 = new Empleado( "Juan", "Pérez","juan.perez@example.com", "contraseñaSegura123", "respuestaSecreta", 30,"123456789", PreguntaSeguridad.Cual_es_el_nombre_de_su_mascota,true );
+        Cliente cliente1 = new Cliente( "Pedro", "Lopez","pedro.lopez@example.com", "contraseñaSegura123", "respuestaSecreta", 30,"123456789", PreguntaSeguridad.Cual_es_el_nombre_de_su_mejor_amigo );
+
+        VehiculoGasolinaDiesel CamionGasolinaDiesel1 = new CamionGasolinaDiesel("ABC123", "Toyota", false, "Camioneta", 4, 200, 300, TipoTransmision.MANUAL, GasolinaDiesel.GASOLINA, true, false, true, 1000, 3, TipoCamion.PLATAFORMA);
+        VehiculoHibrido DeportivoHibrido1 = new DeportivoHibrido("AVB21","HONDA",false,"2023",32,200,250,TipoTransmision.MANUAL,true,true,4,4,2,300,2); ;
         
-        //Empleado empleado1 = new Empleado( "Juan", "Pérez","juan.perez@example.com", "contraseñaSegura123", "respuestaSecreta", 30,"123456789", PreguntaSeguridad.Cual_es_el_nombre_de_su_mascota,true );
-        
+
+
         UQ.agregarAdministrador(administrador1);
-        
+        UQ.agregarEmpleado(empleado1);
+        UQ.agregarCliente(cliente1);
+        UQ.agregarTransaccion(transaccion1);
+        UQ.agregarAlquiler(alquiler1);
+        UQ.agregarCompra(compra1);
+        UQ.agregarVenta(venta1);
+        UQ.agregarVehiculoGasolinaDiesel(CamionGasolinaDiesel1);
+        UQ.agregarVehiculoHibrido(DeportivoHibrido1);
         System.out.println(UQ);
 
 
--- Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/Empresa.java
+++ Concesionario-main/Concesionario-main/concesionarioo2/src/main/java/co/edu/uniquindio/poo/Empresa.java
@@ -10,12 +10,14 @@ public class Empresa {
     public Collection<VentaVehiculo>ventas;
     public Collection<CompraVehiculo>compras;
     public Collection<AlquilerVehiculo>alquilerVehiculos;
-  
-    
- /**
- * Constructor de la clase Empresa
- * @param nombre Nombre de la empresa
- */
+    public Collection<Cliente> clientes;
+    public Collection<Empleado> empleados;
+    public Collection<VehiculoElectrico> vehiculoElectricos;
+    public Collection<VehiculoGasolinaDiesel> VehiculoGasolinaDiesels;
+    public Collection<VehiculoHibrido> vehiculoHibridos;
+
+
+
     public Empresa(String nombre) {
         this.nombre = nombre;
         this.administradores = new LinkedList<>();
@@ -23,107 +25,153 @@ public class Empresa {
         this.ventas = new LinkedList<>();
         this.alquilerVehiculos = new LinkedList<>();
         this.compras = new LinkedList<>();
+        this.clientes = new LinkedList<>();
+        this.empleados = new LinkedList<>();
+        this.vehiculoElectricos = new LinkedList<>();
+        this.VehiculoGasolinaDiesels = new LinkedList<>();
+        this.vehiculoHibridos =new LinkedList<>();
     }
     
     
     
-    /**
- * Método para obtener el nombre de la empresa
- * @return nombre de la empresa
- */
+    
     public String getNombre() {
         return nombre;
     }
 
+    public Collection<VehiculoElectrico> getVehiculoElectricos() {
+        return vehiculoElectricos;
+    }
 
-  /**
- * Método para establecer el nombre de la empresa
- * @param nombre Nombre de la empresa
- */
+
+
+
+
+    public void setVehiculoElectricos(Collection<VehiculoElectrico> vehiculoElectricos) {
+        this.vehiculoElectricos = vehiculoElectricos;
+    }
+
+
+
+
+
+    public Collection<VehiculoGasolinaDiesel> getVehiculoGasolinaDiesels() {
+        return VehiculoGasolinaDiesels;
+    }
+
+
+
+
+
+    public void setVehiculoGasolinaDiesels(Collection<VehiculoGasolinaDiesel> VehiculoGasolinaDiesels) {
+        this.VehiculoGasolinaDiesels = VehiculoGasolinaDiesels;
+    }
+
+
+
+
+
+    public Collection<VehiculoHibrido> getVehiculoHibridos() {
+        return vehiculoHibridos;
+    }
+
+
+
+
+
+    public void setVehiculoHibridos(Collection<VehiculoHibrido> vehiculoHibridos) {
+        this.vehiculoHibridos = vehiculoHibridos;
+    }
+
+
+    public Collection<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+
+
+
+
+    public void setEmpleados(Collection<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+
+
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
 
 
-/**
- * Método para obtener la colección de administradores
- * @return Colección de administradores
- */
 
     public Collection<Administrador> getAdministradores() {
         return administradores;
     }
 
 
-/**
- * Método para establecer la colección de administradores
- * @param administradores Colección de administradores
- */
+
 
     public void setAdministradores(Collection<Administrador> administradores) {
         this.administradores = administradores;
     }
 
 
-/**
- * Método para obtener la colección de transacciones
- * @return Colección de transacciones
- */
+
 
     public Collection<Transaccion> getTransacciones() {
         return transacciones;
     }
 
 
-/**
- * Método para establecer la colección de transacciones
- * @param transacciones Colección de transacciones
- */
+
 
     public void setTransacciones(Collection<Transaccion> transacciones) {
         this.transacciones = transacciones;
     }
 
 
-/**
- * Método para obtener la colección de ventas de vehículos
- * @return Colección de ventas de vehículos
- */
+
 
     public Collection<VentaVehiculo> getVentas() {
         return ventas;
     }
 
 
-/**
- * Método para establecer la colección de ventas de vehículos
- * @param ventas Colección de ventas de vehículos
- */
+
 
     public void setVentas(Collection<VentaVehiculo> ventas) {
         this.ventas = ventas;
     }
 
 
-/**
- * Método para obtener la colección de compras de vehículos
- * @return Colección de compras de vehículos
- */
+
 
     public Collection<CompraVehiculo> getCompras() {
         return compras;
     }
 
 
-/**
- * Método para establecer la colección de compras de vehículos
- * @param compras Colección de compras de vehículos
- */
+
 
     public void setCompras(Collection<CompraVehiculo> compras) {
         this.compras = compras;
     }
+
+    public Collection<Cliente> getCliente() {
+        return clientes;
+    }
+
+
+
+
+
+    public void setCliente(Collection<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    
 
 
 
@@ -139,13 +187,59 @@ public class Empresa {
         this.alquilerVehiculos = alquilerVehiculos;
     }
 
+    public boolean verificarCliente(String cedula) {
+        for (Cliente cliente : clientes) {
+            if (cedula.equals(cliente.getCedula())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void agregarCliente(Cliente cliente){
+        if(verificarCliente(cliente.getCedula())){
+            clientes.add(cliente);
+        }
+    }
+
+    public void eliminarCliente(String cedula) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getCedula().equals(cedula)) {
+                clientes.remove(cliente);
+                break;
+            }
+        }
+    }
+
+    public boolean verificarEmpleado(String Cedula) {
+        for (Empleado empleado : empleados) {
+            if (Cedula.equals(empleado.getCedula())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void agregarEmpleado(Empleado empleado){
+        if(verificarEmpleado(empleado.getCedula())){
+            empleados.add(empleado);
+        }
+    }
+
+    public void eliminarEmpleado(String cedula) {
+        for (Empleado empleado : empleados) {
+            if (empleado.getCedula().equals(cedula)) {
+                empleados.remove(empleado);
+                break;
+            }
+        }
+    }
 
 
-    /**
- * Método para verificar si una transacción existe por su código
- * @param codigo Código de la transacción
- * @return false si la transacción existe, true en caso contrario
- */
+
+
+
+
     public boolean verificarTransaccion(String codigo) {
         for (Transaccion transaccion : transacciones) {
             if (codigo.equals(transaccion.getCodigo())) {
@@ -155,22 +249,11 @@ public class Empresa {
         return true;
     }
 
-
- /**
- * Método para agregar una transacción
- * @param transaccion Objeto Transacción a agregar
- */
-
     public void agregarTransaccion(Transaccion transaccion){
         if(verificarTransaccion(transaccion.getCodigo())){
             transacciones.add(transaccion);
         }
     }
-
-    /**
- * Método para eliminar una transacción
- * @param codigo Código de la transacción a eliminar
- */
 
     public void eliminarTransaccion(String codigo) {
         for (Transaccion transaccion : transacciones) {
@@ -181,12 +264,6 @@ public class Empresa {
         }
     }
 
-    /**
- * Método para verificar si un administrador existe
- * @param cedula Cédula del administrador
- * @return false si el administrador existe, true en caso contrario
- */
-
     public boolean verificarAdministrador(String cedula) {
         for (Administrador administrador : administradores) {
             if (cedula.equals(administrador.getCedula())) {
@@ -196,21 +273,11 @@ public class Empresa {
         return true;
     }
 
-    /**
- * Método para eliminar un administrador de la empresa
- * @param cedula Cédula del administrador a eliminar
- */
-
     public void agregarAdministrador(Administrador administrador){
         if(verificarAdministrador(administrador.getCedula())){
             administradores.add(administrador);
         }
     }
-
-    /**
- * Método para eliminar un administrador de la empresa
- * @param cedula Cédula del administrador a eliminar
- */
 
     public void eliminarAdministrador(String cedula) {
         for (Administrador administrador : administradores) {
@@ -221,11 +288,6 @@ public class Empresa {
         }
     }
 
-    /**
- * Método para agregar una venta a la empresa
- * @param venta Objeto VentaVehiculo a agregar
- */
-
     public boolean verificarVenta(String codigo) {
         for (VentaVehiculo venta : ventas) {
             if (codigo.equals(venta.getCodigo())) {
@@ -235,23 +297,11 @@ public class Empresa {
         return true;
     }
 
-    
-/**
- * Método para eliminar una venta de la empresa
- * @param codigo Código de la venta a eliminar
- */
-
     public void agregarVenta(VentaVehiculo venta){
         if(verificarVenta(venta.getCodigo())){
             ventas.add(venta);
         }
     }
-
-    /**
- * Método para verificar si una compra existe
- * @param codigo Código de la compra
- * @return false si la compra existe, true en caso contrario
- */
 
     public void eliminarVenta(String codigo) {
         for (VentaVehiculo venta : ventas) {
@@ -262,13 +312,6 @@ public class Empresa {
         }
     }
 
-
-    /**
- * Método para verificar si una compra existe
- * @param codigo Código de la compra
- * @return false si la compra existe, true en caso contrario
- */
-
     public boolean verificarCompra(String codigo) {
         for (CompraVehiculo compra : compras) {
             if (codigo.equals(compra.getCodigo())) {
@@ -278,22 +321,11 @@ public class Empresa {
         return true;
     }
 
-    
-/**
- * Método para agregar una compra a la empresa
- * @param compraVehiculo Objeto CompraVehiculo a agregar
- */
-
     public void agregarCompra(CompraVehiculo compraVehiculo){
         if(verificarCompra(compraVehiculo.getCodigo())){
             compras.add(compraVehiculo);
         }
     }
-
-    /**
- * Método para eliminar una compra de la empresa
- * @param codigo Código de la compra a eliminar
- */
 
     public void eliminarCompra(String codigo) {
         for (CompraVehiculo compra : compras) {
@@ -304,12 +336,6 @@ public class Empresa {
         }
     }
 
-    /**
- * Método para verificar si un alquiler existe
- * @param codigo Código del alquiler
- * @return false si el alquiler existe, true en caso contrario
- */
-
     public boolean verificarAlquiler(String codigo) {
         for (AlquilerVehiculo Alquiler : alquilerVehiculos) {
             if (codigo.equals(Alquiler.getCodigo())) {
@@ -319,22 +345,11 @@ public class Empresa {
         return true;
     }
 
-
-    /**
- * Método para agregar un alquiler a la empresa
- * @param Alquiler Objeto AlquilerVehiculo a agregar
- */
-
     public void agregarAlquiler(AlquilerVehiculo Alquiler){
         if(verificarAlquiler(Alquiler.getCodigo())){
             alquilerVehiculos.add(Alquiler);
         }
     }
-
-    /**
- * Método para eliminar un alquiler de la empresa
- * @param codigo Código del alquiler a eliminar
- */
 
     public void eliminarAlquiler(String codigo) {
         for (AlquilerVehiculo Alquiler : alquilerVehiculos) {
@@ -345,13 +360,77 @@ public class Empresa {
         }
     }
 
+    public boolean verificarVehiculoElectrico(String placa) {
+        for (VehiculoElectrico vehiculoElectrico : vehiculoElectricos) {
+            if (placa.equals(vehiculoElectrico.getPlaca())) {
+                return false;
+            }
+        }
+        return true;
+    }
 
- /**
- * Método para actualizar los datos de un cliente
- * @param cedula Cédula del cliente a actualizar
- * @param actualizado Objeto Cliente con los datos actualizados
- * @return true si se actualizó exitosamente, false en caso contrario
- */
+    public void agregarVehiculoElectrico(VehiculoElectrico vehiculoElectrico){
+        if(verificarVehiculoElectrico(vehiculoElectrico.getPlaca())){
+            vehiculoElectricos.add(vehiculoElectrico);
+        }
+    }
+
+    public void eliminarVehiculoElectrico(String placa) {
+        for (VehiculoElectrico vehiculoElectrico : vehiculoElectricos) {
+            if (vehiculoElectrico.getPlaca().equals(placa)) {
+                vehiculoElectricos.remove(vehiculoElectrico);
+                break;
+            }
+        }
+    }
+
+    public boolean verificarVehiculoGasolinaDiesel(String placa) {
+        for (VehiculoGasolinaDiesel VehiculoGasolinaDiesel : VehiculoGasolinaDiesels) {
+            if (placa.equals(VehiculoGasolinaDiesel.getPlaca())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void agregarVehiculoGasolinaDiesel(VehiculoGasolinaDiesel VehiculoGasolinaDiesel){
+        if(verificarVehiculoGasolinaDiesel(VehiculoGasolinaDiesel.getPlaca())){
+            VehiculoGasolinaDiesels.add(VehiculoGasolinaDiesel);
+        }
+    }
+
+    public void eliminarVehiculoGasolinaDiesel(String placa) {
+        for (VehiculoGasolinaDiesel VehiculoGasolinaDiesel : VehiculoGasolinaDiesels) {
+            if (VehiculoGasolinaDiesel.getPlaca().equals(placa)) {
+                VehiculoGasolinaDiesels.remove(VehiculoGasolinaDiesel);
+                break;
+            }
+        }
+    }
+
+    public boolean verificarVehiculoHibrido(String placa) {
+        for (VehiculoHibrido vehiculoHibrido : vehiculoHibridos) {
+            if (placa.equals(vehiculoHibrido.getPlaca())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void agregarVehiculoHibrido(VehiculoHibrido vehiculoHibrido){
+        if(verificarVehiculoHibrido(vehiculoHibrido.getPlaca())){
+            vehiculoHibridos.add(vehiculoHibrido);
+        }
+    }
+
+    public void eliminarVehiculoHibrido(String placa) {
+        for (VehiculoHibrido vehiculoHibrido : vehiculoHibridos) {
+            if (vehiculoHibrido.getPlaca().equals(placa)) {
+                vehiculoHibridos.remove(vehiculoHibrido);
+                break;
+            }
+        }
+    }
 
     public boolean actualizarCliente(String cedula, Cliente actualizado) {
         boolean centinela = false;
@@ -372,13 +451,6 @@ public class Empresa {
         return centinela;
     }
 
-    /**
- * Método para actualizar los datos de un empleado
- * @param cedula Cédula del empleado a actualizar
- * @param actualizado Objeto Empleado con los datos actualizados
- * @return true si se actualizó exitosamente, false en caso contrario
- */
-
     public boolean actualizarEmpleado(String cedula, Empleado actualizado) {
         boolean centinela = false;
         for (Empleado empleado : empleados) {
@@ -398,14 +470,6 @@ public class Empresa {
         }
         return centinela;
     }
-
-
-    /**
- * Método para actualizar los datos de un administrador
- * @param cedula Cédula del administrador a actualizar
- * @param actualizado Objeto Administrador con los datos actualizados
- * @return true si se actualizó exitosamente, false en caso contrario
- */
 
     
     public boolean actualizarAdministrador(String cedula, Administrador actualizado) {
@@ -429,16 +493,13 @@ public class Empresa {
     }
 
 
-    
-/**
- * Método para obtener una representación textual de la empresa
- * @return Representación textual de la empresa
- */
-
-
     @Override
     public String toString() {
         return "Empresa [nombre=" + nombre + ", administradores=" + administradores + ", transacciones=" + transacciones
                 + ", ventas=" + ventas + ", compras=" + compras + ", alquilerVehiculos=" + alquilerVehiculos + "]";
     }
+
+
+
+
 }
